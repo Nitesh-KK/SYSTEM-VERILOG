b4633f286911f503b82592127c67a7cf6c29b274
Nitesh-KK <niteshkk.github@gmail.com>
2026-02-12 22:32:20 +0530

    Create 020. String manipulation methods.sv

---
+++ 02. Arrays/06. String/020. String manipulation methods.sv
@@ -0,0 +1,23 @@
+module String_manipulation;
+  string s;
+  initial begin
+    s="Hello world";
+    $display("String : %s",s);
+    $display("String length: %d",s.len());
+    $display("String upper: %s",s.toupper());
+    $display("String lower: %s",s.tolower());
+    $display("String character: %c",s[0]);
+    $display("Substring: %s",s.substr(0,4));
+  end
+endmodule
+
+/*
+OUTPUT:
+
+String : Hello world
+String length:          11
+String upper: HELLO WORLD
+String lower: hello world
+String character: H
+Substring: Hello
+*/
