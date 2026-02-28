d438743e7154034aa0626a94bee750a05e7843ba
Nitesh-KK <niteshkk.github@gmail.com>
2026-02-28 20:08:04 +0530

    Create 068. Fork_join_any.sv

---
+++ 08. Processes or Threads/068. Fork_join_any.sv
@@ -0,0 +1,36 @@
+module fork_join_any;
+  int a,b;
+  initial begin
+    fork 
+      begin
+      		a=5;
+      		b=10;
+      		#5;
+      		$display("Begin-1:\na=%0d b=%0d",a,b);
+    	end
+    	begin
+      		a=2;
+      		b=5;
+      		#2;
+      		$display("Begin-2:\na=%0d b=%0d",a,b);
+    	end
+    	begin
+      		a=25;
+      		b=15;
+      		$display("Begin-3:\na=%0d b=%0d",a,b);
+          	#4;
+    	end
+    join_any
+    $display("Out of fork_join any");
+  end
+endmodule
+/*
+OUTPUT:
+Begin-3:
+a=25 b=15
+Begin-2:
+a=25 b=15
+Out of fork_join any
+Begin-1:
+a=25 b=15
+*/
